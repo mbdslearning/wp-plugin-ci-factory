39cf093a809640a1cfaf850d505d732231251d36
mbdslearning <mbdslearning@gmail.com>
2026-02-07 07:13:56 +0800

    Add tooling_requirements.py

---
+++ tools/tooling_requirements.py
@@ -0,0 +1,10 @@
+REQ_DEV = {
+    "squizlabs/php_codesniffer": "^3.10",
+    "wp-coding-standards/wpcs": "^3.1",
+    "phpstan/phpstan": "^1.11",
+    "szepeviktor/phpstan-wordpress": "^1.3",
+}
+OPTIONAL_DEV = {
+    "dealerdirect/phpcodesniffer-composer-installer": "^1.0",
+}
+OPTIONAL_ALLOW_PLUGIN = "dealerdirect/phpcodesniffer-composer-installer"
